e1add86d34ae292707c2837c613879c3fd5c91b1
Juraj Petrik <jurajpetrik77@gmail.com>
2015-09-20 11:24:05 -0700

    First Commit

---
+++ Workhous/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Workhous.Startup))]
+namespace Workhous
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

33d8383319f43df064b97b0ceab8768be038a2fd
Juraj Petrik <jurajpetrik77@gmail.com>
2015-09-21 02:53:34 -0700

    Add Models

---
+++ Workhous/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workhous.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double PricePerHour { get; set; }
+        public virtual ICollection<TimeEntry> TimeEntries { get; set; }
+    }
+}
---
+++ Workhous/Models/TimeEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workhous.Models
+{
+    public class TimeEntry
+    {
+        public int ID { get; set; }
+        public DateTime from { get; set; }
+        public DateTime until { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}

efcf068ac01953c920c44841f7489d03c72c0370
Juraj Petrik <jurajpetrik77@gmail.com>
2015-09-30 12:06:49 +0200

    Delete unused view, controller methods that were scaffolded in the beginning

--- Workhous/Controllers/TimeEntriesController.cs
+++ Workhous/Controllers/TimeEntriesController.cs
@@ -14,27 +14,6 @@ namespace Workhous.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: TimeEntries
-        public ActionResult Index()
-        {
-            return View(db.TimeEntries.ToList());
-        }
-
-        // GET: TimeEntries/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TimeEntry timeEntry = db.TimeEntries.Find(id);
-            if (timeEntry == null)
-            {
-                return HttpNotFound();
-            }
-            return View(timeEntry);
-        }
-
         // GET: TimeEntries/Create/1
         public ActionResult Create(int projectId, string projectName)
         {
@@ -58,37 +37,6 @@ namespace Workhous.Controllers
             }
 
             return View(ProjectTimeEntry.TimeEntry);
-        }
-
-        // GET: TimeEntries/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TimeEntry timeEntry = db.TimeEntries.Find(id);
-            if (timeEntry == null)
-            {
-                return HttpNotFound();
-            }
-            return View(timeEntry);
-        }
-
-        // POST: TimeEntries/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,from,until")] TimeEntry timeEntry)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(timeEntry).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(timeEntry);
         }
 
         // GET: TimeEntries/Delete/5
